501e312c90f9bfff31b678e1854a0bb3c2fa25a5
kazawan <55086403@qq.com>
2023-01-06 00:27:49 +0800

    modified:   package.json 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <el-col :span="16" style="text-align: -webkit-center" >
       <H1>KAZA-VUE-TEMPLATE</H1>
       <h2>ELEMENT PLUS+ICON</h2>
-      <el-button type="success">success</el-button>
+      <el-button type="success">修改过</el-button>
     </el-col>
     <el-col :span="4" >
       
